9a56052d3d08ef39514a394b4bc76c43b06b3c6a
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2024-02-02 14:40:45 +0100

    added first look of FillRoute API

---
+++ src/main/java/it/unibo/controller/fillroutecontroller/api/FillRoute.java
@@ -0,0 +1,25 @@
+package it.unibo.controller.fillroutecontroller.api;
+
+import it.unibo.model.player.api.Player;
+import it.unibo.model.route.api.Route;
+
+public interface FillRoute {
+
+    /*
+     * this function returns true if the player has enough train cards with the
+     * color of the chosen route
+     */
+    boolean isRouteValid(Player player, Route route);
+
+    /*
+     * this function opens a pop up if the player can fill a route with both color
+     * tarin cards or jolly train cards and let the player decide
+     */
+    void openPopUp();
+
+    /*
+     * this function sets the color of the button of the chosen route with the
+     * player color
+     */
+    void setColor(Player player, Route route);
+}
