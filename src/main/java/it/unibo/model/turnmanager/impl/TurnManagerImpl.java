ed1507ff4839c66d55d3ced1e54693b485a8b469
Marco Massa <marco.massa6@studio.unibo.it>
2024-02-07 16:37:56 +0100

    added javadocs to turnmanager methods

--- src/main/java/it/unibo/model/turnmanager/impl/TurnManagerImpl.java
+++ src/main/java/it/unibo/model/turnmanager/impl/TurnManagerImpl.java
@@ -17,27 +17,40 @@ public class TurnManagerImpl implements TurnManager {
 
     public TurnManagerImpl(final List<Player> players) {
         this.players = players;
-        setRandomOrder();
+        Collections.shuffle(this.players);
 
         this.playersIterator = this.players.iterator();
         this.currentPlayer = this.playersIterator.next();
         this.currentTurn = 1;
     }
 
-    private void setRandomOrder() {
-        Collections.shuffle(this.players);
-    }
-
+    /**
+     * @return the current player
+     */
     @Override
     public Player getCurrentPlayer() {
         return this.currentPlayer;
     }
 
+    /**
+     * @return the list of players in playing order
+     */
+    @Override
+    public List<Player> getPlayers() {
+        return this.players;
+    }
+
+    /**
+     * @return the current turn
+     */
     @Override
     public int getCurrentTurn() {
         return this.currentTurn;
     }
 
+    /**
+     * Passes control to the next player
+     */
     @Override
     public void switchToNextPlayer() {
         if (!this.playersIterator.hasNext())
@@ -46,6 +59,9 @@ public class TurnManagerImpl implements TurnManager {
         this.currentPlayer = this.playersIterator.next();
     }
 
+    /**
+     * Restart the turn cycle
+     */
     @Override
     public void resetTurns() {
         this.playersIterator = this.players.iterator();
