ae64adaadd73de5ed02ef691865c16bcbbba48c8
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2024-02-17 18:45:51 +0100

    added rules button

--- src/main/java/it/unibo/view/PlayerInterface.java
+++ src/main/java/it/unibo/view/PlayerInterface.java
@@ -1,25 +1,56 @@
 package it.unibo.view;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import it.unibo.controller.gamecontroller.api.MainController;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 
 public class PlayerInterface extends VBox {
 
     public PlayerInterface(final MainController controller, final double width, final double height) {
+        final Screen screen = Screen.getPrimary();
+        final Rectangle2D bounds = screen.getVisualBounds();
         final Button endGame = new Button("End Game");
+        final Button rules = new Button("Rules");
         final Label phase = new Label(controller.getGameController().getPhaseController().toString());
 
         this.getChildren().add(phase);
         this.getChildren().add(endGame);
+        this.getChildren().add(rules);
 
         this.setPadding(new Insets(20));
         this.setSpacing(20);
 
         endGame.setOnAction(event -> {
             controller.getGameController().endGame();
+        });
+
+        rules.setOnAction(event -> {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setResizable(false);
+            alert.getDialogPane().setMaxSize(bounds.getWidth() * 0.6, bounds.getHeight() * 0.5);
+            alert.getDialogPane().setMinSize(bounds.getWidth() * 0.6, bounds.getHeight() * 0.5);
+
+            alert.setTitle("Rules");
+            String rulesText = "";
+            try {
+                rulesText = Files.readString(Path.of("src\\main\\resources\\text\\Rules.txt"), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            alert.setHeaderText(rulesText);
+            alert.showAndWait();
+
         });
 
         final Button endTurn = new Button("End Turn");
