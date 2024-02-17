353e00bbb36a36095ca158d3d7ee6b14a3cb59c8
Orazio Spina <orazio.spina@studio.unibo.it>
2024-02-17 11:26:55 +0100

    Removed test file

--- src/main/java/it/unibo/controller/StartGame.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.controller;
-
-import it.unibo.controller.gamecontroller.impl.MainControllerImpl;
-
-public class StartGame {
-    public static void main(String[] args) {
-        new MainControllerImpl().startView();
-    }
-}
