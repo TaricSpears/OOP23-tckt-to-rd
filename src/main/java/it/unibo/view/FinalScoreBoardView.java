25dedb25edd975c70ec77e4333b8a98e43aa41bb
Marco Massa <marco.massa6@studio.unibo.it>
2024-02-20 18:13:48 +0100

    added background to FinalStage and deleted 2 unusued api

--- src/main/java/it/unibo/view/FinalScoreBoardView.java
+++ src/main/java/it/unibo/view/FinalScoreBoardView.java
@@ -10,9 +10,18 @@ import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -26,7 +35,7 @@ import javafx.stage.Stage;
  */
 public class FinalScoreBoardView extends Stage {
 
-    private static final double SCENE_WIDTH_SCALE = 0.2;
+    private static final double SCENE_WIDTH_SCALE = 0.3;
     private static final double SCENE_HEIGHT_SCALE = 0.5;
     private static final int FONT_SIZE = 25;
     private static final int TOP_BOTTOM_PADDING = 15;
@@ -58,6 +67,13 @@ public class FinalScoreBoardView extends Stage {
         final BorderPane root = new BorderPane();
         final VBox playersList = new VBox();
         final HBox gameControls = new HBox();
+        final BorderPane pane = new BorderPane();
+
+        final Image image = new Image("/img/Wallpapers/Wallpaper1.jpg");
+        final BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                new BackgroundSize(1, 1, true, true, false, false));
+        root.setBackground(new Background(backgroundImage));
 
         gameControls.getChildren().addAll(newGame, endButton);
         gameControls
@@ -68,11 +84,10 @@ public class FinalScoreBoardView extends Stage {
         final List<Pair<String, Double>> players = controller.getGameController().getScore();
 
         for (final Pair<String, Double> player : players) {
-            final Text playerEntry = new Text(player.first() + " - " + player.second());
+            final Text playerEntry = new Text(player.first() + " : " + player.second());
 
             playerEntry.setTextAlignment(TextAlignment.CENTER);
             playerEntry.setFont(Font.font("helvetica", FontWeight.BOLD, FontPosture.REGULAR, FONT_SIZE));
-
             playersList.getChildren().add(playerEntry);
         }
 
@@ -80,7 +95,14 @@ public class FinalScoreBoardView extends Stage {
                 .setPadding(new Insets(TOP_BOTTOM_PADDING, LEFT_RIGHT_PADDING, TOP_BOTTOM_PADDING, LEFT_RIGHT_PADDING));
         playersList.setAlignment(Pos.CENTER);
 
-        root.setCenter(playersList);
+        final BackgroundFill backgroundFill = new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY);
+        final Background background = new Background(backgroundFill);
+
+        pane.setBackground(background);
+        pane.setCenter(playersList);
+        pane.setMaxHeight(bounds.getHeight() * SCENE_HEIGHT_SCALE * SCENE_HEIGHT_SCALE * SCENE_HEIGHT_SCALE);
+        pane.setMaxWidth(bounds.getWidth() * SCENE_WIDTH_SCALE * SCENE_WIDTH_SCALE * SCENE_WIDTH_SCALE);
+        root.setCenter(pane);
         root.setBottom(gameControls);
 
         this.setScene(new Scene(root, bounds.getWidth() * SCENE_WIDTH_SCALE, bounds.getHeight() * SCENE_HEIGHT_SCALE));
