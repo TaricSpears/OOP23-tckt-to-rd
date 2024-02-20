6902454e0e5cdd5c805bc61e2fbf6f6a25f7021f
Orazio Spina <orazio.spina@studio.unibo.it>
2024-02-20 21:23:48 +0100

    Fixed some more gradle build errors

--- src/main/java/it/unibo/view/LastTurnPopUp.java
+++ src/main/java/it/unibo/view/LastTurnPopUp.java
@@ -13,18 +13,26 @@ public class LastTurnPopUp {
     private static final double WIDTH = 300;
     private static final double HEIGHT = 200;
 
+    private final Alert alert = new Alert(AlertType.INFORMATION);
+
     /**
      * Opens the popUp.
      */
     public LastTurnPopUp() {
 
-        final Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Ultimo turno");
         alert.setContentText("Ultimo turno di gioco!");
 
         alert.setResizable(false);
         alert.getDialogPane().setMaxSize(WIDTH, HEIGHT);
         alert.getDialogPane().setMinSize(WIDTH, HEIGHT);
+        alert.showAndWait();
+    }
+
+    /**
+     * Displays the popUp.
+     */
+    public void display() {
         alert.showAndWait();
     }
 }
