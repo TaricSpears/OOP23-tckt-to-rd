f10f569bed9134a8bcb500e9b83173420acc8537
Orazio Spina <orazio.spina@studio.unibo.it>
2024-02-20 18:50:20 +0100

    Fixed some more gradle build errors

--- src/test/java/it/unibo/scorecalculator/TestScoreCalculator.java
+++ src/test/java/it/unibo/scorecalculator/TestScoreCalculator.java
@@ -3,55 +3,33 @@ package it.unibo.scorecalculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
-import it.unibo.commons.EdgeData;
 import it.unibo.commons.Pair;
-import it.unibo.model.city.api.City;
-import it.unibo.model.city.impl.CityImpl;
+import it.unibo.commons.TestDataPreparation;
 import it.unibo.model.gameprep.impl.GamePrep;
 import it.unibo.model.player.api.Player;
 import it.unibo.model.route.api.Route;
-import it.unibo.model.route.impl.RouteImpl;
 import it.unibo.model.scorecalculator.api.ScoreCalculator;
 import it.unibo.model.scorecalculator.impl.ScoreCalculatorImpl;
-
-import java.awt.Color;
 
 /**
  * This class is used for testing the ScoreCalculator.
  */
 class TestScoreCalculator {
-    private final List<Pair<String, Color>> playerData = List.of(new Pair<String, Color>("Player1", Color.RED),
-            new Pair<String, Color>("Player2", Color.BLUE), new Pair<String, Color>("Player3", Color.GREEN),
-            new Pair<String, Color>("Player4", Color.YELLOW), new Pair<String, Color>("Player5", Color.BLACK),
-            new Pair<String, Color>("Player6", Color.ORANGE));
-
-    private final City city1 = new CityImpl("Rome");
-    private final City city2 = new CityImpl("Milan");
-    private final City city3 = new CityImpl("Naples");
-
-    private final Route route1 = new RouteImpl(new EdgeData(city1, city2, 5), Color.RED, 0, null);
-    private final Route route2 = new RouteImpl(new EdgeData(city1, city3, 3), Color.BLACK, 1, null);
-    private final Route route3 = new RouteImpl(new EdgeData(city2, city3, 7), Color.GREEN, 2, null);
-
-    private final List<Route> routeData = List.of(route1, route2, route3);
-
-    private final GamePrep gamePrep = new GamePrep();
-
-    TestScoreCalculator() {
-        gamePrep.prepGame(playerData, routeData);
-    }
-
     @Test
     void testGetScoreBoard() {
+        final GamePrep gamePrep = TestDataPreparation.testPrep();
         final ScoreCalculator scoreCalculator = new ScoreCalculatorImpl();
         final List<Player> players = gamePrep.getPlayers();
 
-        players.get(0).addRoute(route1);
-        players.get(1).addRoute(route2);
-        players.get(2).addRoute(route3);
+        final Set<Route> routes = gamePrep.getGraph().edgeSet();
+
+        for (int i = 0; i < routes.size(); i++) {
+            players.get(i).addRoute((Route) routes.toArray()[i]);
+        }
 
         final List<Pair<String, Double>> score = scoreCalculator.getScoreBoard(gamePrep.getPlayers());
 
