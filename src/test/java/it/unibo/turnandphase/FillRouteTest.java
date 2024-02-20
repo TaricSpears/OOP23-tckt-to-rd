5c329877ecae431dc4949052090c95d34444fcbe
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2024-02-20 17:26:23 +0100

    added Wallpapers jpgs

--- src/test/java/it/unibo/turnandphase/FillRouteTest.java
+++ src/test/java/it/unibo/turnandphase/FillRouteTest.java
@@ -4,12 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import it.unibo.commons.EdgeData;
+import it.unibo.commons.Pair;
 import it.unibo.commons.Region;
 import it.unibo.controller.fillroutecontroller.impl.FillRouteImpl;
 import it.unibo.controller.gamecontroller.api.MainController;
 import it.unibo.controller.gamecontroller.impl.MainControllerImpl;
+import it.unibo.controller.phasecontroller.api.PhaseController;
+import it.unibo.model.city.impl.CityImpl;
+import it.unibo.model.gameprep.impl.GamePrep;
 import it.unibo.model.player.api.Player;
 import it.unibo.model.player.impl.PlayerImpl;
+import it.unibo.model.route.api.Route;
+import it.unibo.model.route.impl.RouteImpl;
+import java.util.List;
+import java.awt.Color;
 
 /**
  * This class is used to test the FillRoute class.
@@ -24,10 +33,15 @@ public class FillRouteTest {
     @Test
     public void testFillRouteEmpty() {
         // Test code goes here
-        MainController controller = new MainControllerImpl();
+        GamePrep gamePrep = new GamePrep();
         Player player = new PlayerImpl(null, null, 0);
         Region region = new Region(0, 0, 0, 0, 0, 0, null, null);
-        FillRouteImpl fillRouteImpl = new FillRouteImpl(player, region, controller);
+        Route route = new RouteImpl(new EdgeData(new CityImpl(null), new CityImpl(null), 5), null, 0, null);
+        gamePrep.prepGame(List.of(new Pair<String, Color>(player.getName(), player.getColor())), List.of(route));
+        MainController controller = new MainControllerImpl();
+        controller.startGame();
+        PhaseController phase = controller.getPhaseController();
+        FillRouteImpl fillRouteImpl = new FillRouteImpl();
         assertEquals(false, fillRouteImpl.isRouteValid());
 
     }
