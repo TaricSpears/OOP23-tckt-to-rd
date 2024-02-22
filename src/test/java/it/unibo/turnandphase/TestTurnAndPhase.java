2acc3ae02b1f1da56a2773634b5060cf1b41da33
Lorenzo Bergami <lorebergam@gmail.com>
2024-02-22 16:09:24 +0100

    removed comments in TestTurnAndPhase

--- src/test/java/it/unibo/turnandphase/TestTurnAndPhase.java
+++ src/test/java/it/unibo/turnandphase/TestTurnAndPhase.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-// import java.util.List;
-// import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 
-// import it.unibo.model.player.api.Player;
-// import it.unibo.model.player.impl.PlayerImpl;
-// import it.unibo.model.turnmanager.api.TurnManager;
-// import it.unibo.model.turnmanager.impl.TurnManagerImpl;
+import it.unibo.model.player.api.Player;
+import it.unibo.model.player.impl.PlayerImpl;
+import it.unibo.model.turnmanager.api.TurnManager;
+import it.unibo.model.turnmanager.impl.TurnManagerImpl;
 import it.unibo.model.phasemanager.api.PhaseManager;
 import it.unibo.model.phasemanager.impl.PhaseManagerImpl;
 
@@ -19,41 +19,40 @@ import it.unibo.model.phasemanager.impl.PhaseManagerImpl;
  */
 class TestTurnAndPhase {
 
-    /*
-     * @Test
-     * void testTurnManager() {
-     * 
-     * List<Player> players = new ArrayList<>();
-     * final int carriageNum = 45;
-     * players.add(new PlayerImpl("Player 1", carriageNum));
-     * players.add(new PlayerImpl("Player 2", carriageNum));
-     * players.add(new PlayerImpl("Player 3", carriageNum));
-     * players.add(new PlayerImpl("Player 4", carriageNum));
-     * 
-     * final TurnManager turnManager = new TurnManagerImpl(players);
-     * players = new ArrayList<>(turnManager.getPlayers());
-     * 
-     * assertEquals(1, turnManager.getCurrentTurn());
-     * assertEquals(players.get(0), turnManager.getCurrentPlayer());
-     * 
-     * turnManager.switchToNextPlayer();
-     * assertEquals(1, turnManager.getCurrentTurn());
-     * assertEquals(players.get(1), turnManager.getCurrentPlayer());
-     * 
-     * turnManager.switchToNextPlayer();
-     * assertEquals(1, turnManager.getCurrentTurn());
-     * assertEquals(players.get(2), turnManager.getCurrentPlayer());
-     * 
-     * turnManager.switchToNextPlayer();
-     * assertEquals(1, turnManager.getCurrentTurn());
-     * assertEquals(players.get(3), turnManager.getCurrentPlayer());
-     * 
-     * turnManager.switchToNextPlayer();
-     * assertEquals(2, turnManager.getCurrentTurn());
-     * assertEquals(players.get(0), turnManager.getCurrentPlayer());
-     * 
-     * }
-     */
+    @Test
+    void testTurnManager() {
+    
+    List<PlayerImpl> players = new ArrayList<>();
+    final int carriageNum = 45;
+    players.add(new PlayerImpl("Player 1", carriageNum));
+    players.add(new PlayerImpl("Player 2", carriageNum));
+    players.add(new PlayerImpl("Player 3", carriageNum));
+    players.add(new PlayerImpl("Player 4", carriageNum));
+    
+    final TurnManager turnManager = new TurnManagerImpl(players);
+    List<Player> players2 = new ArrayList<>(turnManager.getPlayers());
+    
+    assertEquals(1, turnManager.getCurrentTurn());
+    assertEquals(players2.get(0), turnManager.getCurrentPlayer());
+    
+    turnManager.switchToNextPlayer();
+    assertEquals(1, turnManager.getCurrentTurn());
+    assertEquals(players2.get(1), turnManager.getCurrentPlayer());
+    
+    turnManager.switchToNextPlayer();
+    assertEquals(1, turnManager.getCurrentTurn());
+    assertEquals(players2.get(2), turnManager.getCurrentPlayer());
+    
+    turnManager.switchToNextPlayer();
+    assertEquals(1, turnManager.getCurrentTurn());
+    assertEquals(players2.get(3), turnManager.getCurrentPlayer());
+    
+    turnManager.switchToNextPlayer();
+    assertEquals(2, turnManager.getCurrentTurn());
+    assertEquals(players2.get(0), turnManager.getCurrentPlayer());
+    
+    }
+    
     @Test
     void testPhaseManager() {
 
